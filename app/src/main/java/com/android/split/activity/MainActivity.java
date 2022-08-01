123807c9b568981925e065851cacf64dff0e5eb8
john0227 <37623568+john0227@users.noreply.github.com>
2022-08-02 03:15:56 +0800

    Created Java classes for each Fragment

--- app/src/main/java/com/android/split/activity/MainActivity.java
+++ app/src/main/java/com/android/split/activity/MainActivity.java
@@ -1,17 +1,37 @@
 package com.android.split.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.android.library.log.LogService;
 import com.android.split.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager2 vp2_split;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        try {
+            setContentView(R.layout.activity_main);
+
+            init();
+            setting();
+        } catch (Exception e) {
+            LogService.error(this, e.getMessage(), e);
+        }
+    }
+
+    private void init() {
+        this.vp2_split = findViewById(R.id.vp2_split);
+    }
+
+    private void setting() {
+
     }
 
 }
