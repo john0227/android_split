096d7168aea439c17802dc94ec17e1b19e936dad
john0227 <37623568+john0227@users.noreply.github.com>
2022-08-02 01:59:00 +0800

    Add existing project files to Github

---
+++ app/src/main/java/com/android/split/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.android.split.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.split.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
