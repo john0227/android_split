55c49dadc9e217add2216b8bf3ce757b4e7be78e
john0227 <37623568+john0227@users.noreply.github.com>
2022-08-02 17:52:09 +0800

    Added navigation for ViewPager2

--- app/src/main/java/com/android/split/activity/MainActivity.java
+++ app/src/main/java/com/android/split/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.android.split.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -12,16 +14,21 @@ import com.android.split.adapter.MyPagerAdapter;
 import com.android.split.fragment.NameFragment;
 import com.android.split.fragment.ResultFragment;
 import com.android.split.fragment.TransactionFragment;
+import com.android.split.logic.Logic;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 vp2_split;
-    private MyPagerAdapter pagerAdapter;
+    private Button btn_back;
+    private Button btn_next;
 
     private Fragment[] fragments;
     private NameFragment nameFragment;
     private TransactionFragment transactionFragment;
     private ResultFragment resultFragment;
+
+    private MyPagerAdapter pagerAdapter;
+    private Logic logic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         this.vp2_split = findViewById(R.id.vp2_split);
+        this.btn_back = findViewById(R.id.btn_back);
+        this.btn_next = findViewById(R.id.btn_next);
 
         this.nameFragment = new NameFragment();
         this.transactionFragment = new TransactionFragment();
@@ -55,6 +64,25 @@ public class MainActivity extends AppCompatActivity {
     private void setting() {
         this.vp2_split.setAdapter(this.pagerAdapter);
         this.vp2_split.setUserInputEnabled(false);
+        this.vp2_split.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                setEnabled(btn_back, position > 0);
+                setEnabled(btn_next, position < 2);
+                btn_next.setText(position != 1
+                        ? R.string.btn_next_text1
+                        : R.string.btn_next_text2
+                );
+            }
+        });
+
+        this.btn_back.setOnClickListener(view -> this.vp2_split.setCurrentItem(this.vp2_split.getCurrentItem() - 1));
+        this.btn_next.setOnClickListener(view -> this.vp2_split.setCurrentItem(this.vp2_split.getCurrentItem() + 1));
+    }
+
+    private void setEnabled(Button button, boolean enabled) {
+        button.setVisibility(enabled ? View.VISIBLE : View.INVISIBLE);
+        button.setEnabled(enabled);
     }
 
 }
