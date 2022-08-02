860855407724264cee1b58aa18063988429fad05
john0227 <37623568+john0227@users.noreply.github.com>
2022-08-03 00:17:54 +0800

    Updated style of Spinner and AlertDialog

--- app/src/main/java/com/android/split/dialog/WarningDialog.java
+++ app/src/main/java/com/android/split/dialog/WarningDialog.java
@@ -3,10 +3,12 @@ package com.android.split.dialog;
 import android.app.Activity;
 import android.app.AlertDialog;
 
+import com.android.split.R;
+
 public class WarningDialog {
 
     public static void show(Activity activity, String title, String msg, String cancel) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity, R.style.MyDialogTheme);
 
         builder.setTitle(title);
         builder.setMessage(msg);
